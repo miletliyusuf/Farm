3065a82bd63dfcef89ea8cd993a4c980d57fb9d5
Yusuf Miletli <miletliyusuf@gmail.com>
2022-02-08 11:52:22 +0300

    Init farm project

---
+++ Farm/Farm/FarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmApp.swift
+//  Farm
+//
+//  Created by Yusuf Miletli on 8.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct FarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
